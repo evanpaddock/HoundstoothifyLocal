624f9ec8eaca5655041d66f5e9dd545bf522201e
evanpaddock <edpaddock@crimson.ua.edu>
2023-02-14 01:00:42 -0600

    Houndstoothify Deploy v1.0

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -6,7 +6,6 @@ AddNewSongBySubmit();
 function AddNewSongBySubmit(){
     let form = document.getElementById("newSongForm");
     form.addEventListener('submit',function(e){
-        e.preventDefault();
         let currentDate = new Date().toLocaleString().slice(0,9);
         let newSong = {
             title: e.target.elements.songTitle.value,
@@ -19,6 +18,7 @@ function AddNewSongBySubmit(){
                 .toString(16)
             )
         }
+
         let songTitleIndex = allsongs.findIndex(function(element){
             if (element.title == newSong.title) {
                 return indexedDB;
@@ -34,17 +34,16 @@ function AddNewSongBySubmit(){
             if(newSong.artist === allsongs[songArtistIndex].artist && newSong.title ===allsongs[songTitleIndex].title){
                 allsongs[songTitleIndex].deleted = false;
                 allsongs[songTitleIndex].favorited = false;
-                localStorage.setItem("Houndstoothify-Songs", JSON.stringify(allsongs));
+                localStorage.setItem("1", JSON.stringify(allsongs));
                 var elementExists = document.getElementById(`${allsongs[songTitleIndex].title}`);
-                
                 if(elementExists == null){
                     window.location.reload();
                 }
         }
         }catch{
-            
+            e.preventDefault();
             allsongs.push(newSong);
-            localStorage.setItem("Houndstoothify-Songs", JSON.stringify(allsongs));
+            localStorage.setItem("1", JSON.stringify(allsongs));
             CreateNewCard(newSong);
         }
         e.target.elements.songTitle.value = null;
@@ -54,16 +53,16 @@ function AddNewSongBySubmit(){
 
 function CreateNewCard(song){
     document.getElementById("songCards")
-    .insertAdjacentHTML("afterbegin",`<div class="col-md-4"><div class="card mb-4 shadow-sm"><div class="card-body"><p class="card-text">Title: ${song.title}<br>Artist: ${song.artist}<br>Date Added: ${song.date}<br>Favorited: ${song.favorited}<div class="d-flex justify-content-between align-items-center"><div class="btn-group" id = "${song.title}"><button type="button" value = "submit" onClick = "FavoriteASong()" class="btn btn-sm btn-outline-secondary" id = "${song.title}">Favorite</button><button type="button" value = "submit" class="btn btn-sm btn-outline-secondary" value = "submit" onClick = "DeleteASong()" id="delete">Delete</button></div></div></div></div></div>`);
+    .insertAdjacentHTML("afterbegin",`<div class="col-md-4"><div class="card mb-4 shadow-sm"><div class="card-body"><p class="card-text">Title: ${song.title}<br>Artist: ${song.artist}<br>Date Added: ${song.date}<br>Favorited: ${song.favorited}<div class="d-flex justify-content-between align-items-center"><div class="btn-group" id = "${song.ID}"><button type="button" value = "submit" onClick = "FavoriteASong()" class="btn btn-sm btn-outline-secondary" id = "${song.ID}">Favorite</button><button type="button" value = "submit" class="btn btn-sm btn-outline-secondary" value = "submit" onClick = "DeleteASong()" id="delete">Delete</button></div></div></div></div></div>`);
 }
 
 function OnLoad(){
     //read in array of objects
-   let allsongs = JSON.parse(localStorage.getItem("Houndstoothify-Songs"))
+   let allsongs = JSON.parse(localStorage.getItem("1"))
    try{let success = allsongs[0].deleted}
    catch{
     allsongs = []
-    alert("Songs failed to load or are not available.\nAdd a song to enable song storage.")
+    alert("Songs failed to load or are not available.\nAdd a song to enable song stroage.")
     }
 
     //write all cards for objects
@@ -78,10 +77,10 @@ function OnLoad(){
 }
 
 function FavoriteASong(){
-    let favoriteName = event.target.parentNode.id;
+    let ID = event.target.parentNode.id;
 
     let foundIndex = allsongs.findIndex(function(element){
-        if (element.title == favoriteName) {
+        if (element.ID == ID) {
             return indexedDB;
         }
     })
@@ -92,16 +91,16 @@ function FavoriteASong(){
         allsongs[foundIndex].favorited = true;
     }
 
-    localStorage.setItem("Houndstoothify-Songs", JSON.stringify(allsongs));
+    localStorage.setItem("1", JSON.stringify(allsongs));
 
     window.location.reload();
 }
 
 function DeleteASong(){
-    let favoriteName = event.target.parentNode.id;
+    let ID = event.target.parentNode.id;
 
     let foundIndex = allsongs.findIndex(function(element){
-        if (element.title == favoriteName) {
+        if (element.ID == ID) {
             return indexedDB;
         }
     })
@@ -112,7 +111,7 @@ function DeleteASong(){
         allsongs[foundIndex].deleted = true;
     }
 
-    localStorage.setItem("Houndstoothify-Songs", JSON.stringify(allsongs));
+    localStorage.setItem("1", JSON.stringify(allsongs));
 
     window.location.reload();
 }
